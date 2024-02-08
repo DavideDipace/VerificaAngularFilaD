85a829674a8379fa4c1984534dde13f2d02f4147
DavideDipace <dipace.davide@iisgalvanimi.edu.it>
2024-02-08 13:54:14 +0000

    finire terzo commit

--- VerificafilaD/src/app/app.component.ts
+++ VerificafilaD/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'VerificafilaD';
+
+  end : string
+  macchina(colore : HTMLInputElement, porte : HTMLInputElement, cambio : HTMLInputElement): boolean
+
+  this.end = 
 }
